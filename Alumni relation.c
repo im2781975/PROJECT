b750b3313f54bd7117beadcaeb18e5e1651b5b0e
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2023-12-14 17:52:31 +0100

    Update Alumni relation.c

--- Alumni relation.c
+++ Alumni relation.c
@@ -19,72 +19,84 @@ void display(struct student s) {
 }
 
 int main() {
+    int flag=0;
+    char ch;
     int n, id;
     char Username[30];
     char pass[50];
     int choice;
 
-    printf("Enter choice: ");
-    scanf("%d", &choice);
-    printf("\nEnter username: ");
-    scanf("%s", Username);
-    printf("\nEnter password: ");
-    scanf("%s", pass);
+    do {
+        printf("Enter choice: ");
+        scanf("%d", &choice);
+        printf("\nEnter username: ");
+        scanf("%s", Username);
+        printf("\nEnter password: ");
+        scanf("%s", pass);
 
-    switch (choice) {
-        case 1: {
-            if (strcmp(Username, "Admin") == 0 && strcmp(pass, "1234") == 0) {
-                printf("Enter number of students: ");
-                scanf("%d", &n);
-                struct student s[n];
+        switch (choice) {
+            case 1: {
+                if (strcmp(Username, "Admin") == 0 && strcmp(pass, "1234") == 0) {
+                    printf("Enter number of students: ");
+                    scanf("%d", &n);
+                    struct student s[n];
 
-                printf("Information of students: \n");
-                for (int i = 0; i < n; i++) {
-                    printf("\nEnter Name of no.%d: ", i + 1);
-                    scanf("%s", s[i].name);
-                    printf("Enter contact of no.%d: ", i + 1);
-                    scanf("%s", s[i].contact);
-                    printf("Enter status of no.%d: ", i + 1);
-                    scanf("%s", s[i].status);
-                    printf("Enter achive of no.%d: ", i + 1);
-                    scanf("%s", s[i].achive);
-                    s[i].id = 112310092+i;
-                }
-
-                printf("Enter id: ");
-                scanf("%d", &id);
-
-                for (int i = 0; i < n; i++) {
-                    if (id == s[i].id) {
-                        display(s[i]);
-                        return 0;
+                    printf("Information of students: \n");
+                    for (int i = 0; i < n; i++) {
+                        printf("\nEnter Name of no.%d: ", i + 1);
+                        scanf("%s", s[i].name);
+                        printf("Enter contact of no.%d: ", i + 1);
+                        scanf("%s", s[i].contact);
+                        printf("Enter status of no.%d: ", i + 1);
+                        scanf("%s", s[i].status);
+                        printf("Enter achive of no.%d: ", i + 1);
+                        scanf("%s", s[i].achive);
+                        s[i].id = 112310092 + i;
                     }
-                }
-                printf("\nSortage of information.");
-            } else {
-                printf("\nPlease enter valid username & password");
-            }
-            break;
-        }
-        default: {
-            if (strcmp(Username, "Student") == 0 && strcmp(pass, "2345") == 0) {
-                printf("Enter id: ");
-                scanf("%d", &id);
-                struct student s; // Declare a single student outside the loop
 
-                for (int i = 0; i < n; i++) {
-                    if (id == s.id)    {
-                        display(s);
-                        break;
+                    printf("Enter id: ");
+                    scanf("%d", &id);
+
+                    for (int i = 0; i < n; i++) {
+                        if (id == s[i].id) {
+                            display(s[i]);
+                            flag=1;
+                            break;
+                            
+                        }
                     }
-                    
+                } else {
+                    printf("\nPlease enter valid username & password");
                 }
-                printf("\nSortage of information.");
-            } else {
-                printf("\nPlease enter valid username & password");
+                break;
             }
-            break;
+            default: {
+                if (strcmp(Username, "Student") == 0 && strcmp(pass, "2345") == 0) {
+                    printf("Enter id: ");
+                    scanf("%d", &id);
+                    struct student s; // Declare a single student outside the loop
+
+                    for (int i = 0; i < n; i++) {
+                        if (id == s.id) {
+                            display(s);
+                            flag=1;
+                            break;
+                        }
+                    }
+                } else {
+                    printf("\nPlease enter valid username & password");
+                    flag==2;
+                }
+                break;
+            }
         }
-    }
+        if(flag==0)
+        {
+            printf("Sortage of information");
+        }
+        printf("\nEnter('1' for continue) : ");
+        scanf(" %c", &ch); // Corrected format specifier for char input
+    } while (ch == '1');
+
     return 0;
 }
